fc5d20ed0f6cc2925bed9e0140d02daff8e9ddd7
alejandro-pascual <alejandro.pascual@gracia.lasalle.cat>
2024-01-08 16:11:00 +0100

    Change button function

--- src/main/java/com/example/javafxproject/HelloController.java
+++ src/main/java/com/example/javafxproject/HelloController.java
@@ -1,14 +1,21 @@
 package com.example.javafxproject;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.lang.reflect.Field;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField userName;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onButtonClick() {
+        welcomeText.setText(userName.getText());
+
     }
 }
